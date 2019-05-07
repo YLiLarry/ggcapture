902005397a13db243fc8f79d9523e644009fb121
Yu Li <YLiLarry@gmail.com>
2019-05-07 01:46:37 -0400

    whitespace changes

--- example/ggcapture_example.cc
+++ example/ggcapture_example.cc
@@ -11,41 +11,41 @@ using namespace ggframe;
 class CustomCapture : public GGCapture
 {
 private:
-	atomic<int> m_fps = 0;
-	unique_ptr<thread> m_tick_interval;
+    atomic<int> m_fps = 0;
+    unique_ptr<thread> m_tick_interval;
 protected:
-	void newFrameArrived(shared_ptr<ggframe::Frame> frame) override
-	{
-		m_fps++;
-		cerr << "+";
-		GGCapture::newFrameArrived(frame);
-	}
+    void newFrameArrived(shared_ptr<ggframe::Frame> frame) override
+    {
+        m_fps++;
+        cerr << "+";
+        GGCapture::newFrameArrived(frame);
+    }
 public:
-	CustomCapture() : GGCapture() { 
-		m_tick_interval = make_unique<thread>([&]() {
-			while(true) {
-				this_thread::sleep_for(chrono::seconds(1));
-				cerr << m_fps << " fps" << endl;
-				m_fps = 0;
-			}
-		});
-	}
-	void start(string title, GGCapture::CaptureMethod mode, int fps) {
-		GGCapture::start(title, mode, fps);
-		m_tick_interval->detach();
-	}
+    CustomCapture() : GGCapture() { 
+        m_tick_interval = make_unique<thread>([&]() {
+            while(true) {
+                this_thread::sleep_for(chrono::seconds(1));
+                cerr << m_fps << " fps" << endl;
+                m_fps = 0;
+            }
+        });
+    }
+    void start(string title, GGCapture::CaptureMethod mode, int fps) {
+        GGCapture::start(title, mode, fps);
+        m_tick_interval->detach();
+    }
 };
 
 int main()
 {
-	CustomCapture ggcapture;
+    CustomCapture ggcapture;
 #if WIN32
-	ggcapture.start("task manager", GGCapture::DirectXDesktopDuplication, 60);
+    ggcapture.start("task manager", GGCapture::DirectXDesktopDuplication, 60);
 #elif APPLE 
-	ggcapture.start("activity monitor", GGCapture::Window, 30);
+    ggcapture.start("activity monitor", GGCapture::Window, 30);
 #endif
-	ggcapture.showFrame();
-	cerr << "press any key to exit" << endl;
-	string in;
-	cin >> in; 
+    ggcapture.showFrame();
+    cerr << "press any key to exit" << endl;
+    string in;
+    cin >> in; 
 }
--- include/ggcapture.h
+++ include/ggcapture.h
@@ -14,76 +14,76 @@
 
 namespace ggcapture {
 
-	using namespace std;
-	using namespace SL::Screen_Capture;
-	using namespace SL;
-	using ggframe::Frame;	
+    using namespace std;
+    using namespace SL::Screen_Capture;
+    using namespace SL;
+    using ggframe::Frame;	
 
 #if APPLE
-	using boost::filesystem::path;
-	using std::shared_ptr;
+    using boost::filesystem::path;
+    using std::shared_ptr;
 #else
-	using std::filesystem::path;
+    using std::filesystem::path;
 #endif
 
-	class GGCapture {
-	public:
-		enum CaptureMethod {
+    class GGCapture {
+    public:
+        enum CaptureMethod {
 #if WIN32
-			DirectXDesktopDuplication,
+            DirectXDesktopDuplication,
 #endif
-			Window
-		};
-		enum CaptureStatus {
-			Stopped,
-			Capturing
-		};
+            Window
+        };
+        enum CaptureStatus {
+            Stopped,
+            Capturing
+        };
 
-	private:
-		atomic<CaptureMethod> m_capture_mode = CaptureMethod::Window;
-		atomic<CaptureStatus >m_status = Stopped;
-		atomic<bool> m_show_frame_flag = false;
-		atomic<bool> m_save_frame_flag = false;
-		atomic<thread::id> m_capture_thread_id;
-		string m_window_title; /* capture thread */
-		path m_storage_path = "./data"; /* capture thread */
-		int m_capture_fps; /* capture thread */
-		shared_ptr<ICaptureConfiguration<ScreenCaptureCallback>> m_screen_capture_config; /* capture thread */
-		shared_ptr<ICaptureConfiguration<WindowCaptureCallback>> m_window_capture_config; /* capture thread */
-		shared_ptr<IScreenCaptureManager> m_capture_manager; /* capture thread */
-		Screen_Capture::Window m_window;  /* capture thread only */
-		mutex m_frame_queue_mutex;
-		queue<shared_ptr<Frame>> m_frame_queue; /* capture thread */
+    private:
+        atomic<CaptureMethod> m_capture_mode = CaptureMethod::Window;
+        atomic<CaptureStatus >m_status = Stopped;
+        atomic<bool> m_show_frame_flag = false;
+        atomic<bool> m_save_frame_flag = false;
+        atomic<thread::id> m_capture_thread_id;
+        string m_window_title; /* capture thread */
+        path m_storage_path = "./data"; /* capture thread */
+        int m_capture_fps; /* capture thread */
+        shared_ptr<ICaptureConfiguration<ScreenCaptureCallback>> m_screen_capture_config; /* capture thread */
+        shared_ptr<ICaptureConfiguration<WindowCaptureCallback>> m_window_capture_config; /* capture thread */
+        shared_ptr<IScreenCaptureManager> m_capture_manager; /* capture thread */
+        Screen_Capture::Window m_window;  /* capture thread only */
+        mutex m_frame_queue_mutex;
+        queue<shared_ptr<Frame>> m_frame_queue; /* capture thread */
 
 #if APPLE
-		atomic<float> m_pixel_density = 1; /* capture thread */
+        atomic<float> m_pixel_density = 1; /* capture thread */
 #else
-		atomic<float> m_pixel_density = 1; /* capture thread */
+        atomic<float> m_pixel_density = 1; /* capture thread */
 #endif
 
-		void setWindowTitle(string srchterm); /* capture thread */
-		void setCaptureMode(CaptureMethod mode); /* capture thread */
-		void initCaptureConfig(); /* capture thread */
-		void setCaptureFps(int fps); /* capture thread */
+        void setWindowTitle(string srchterm); /* capture thread */
+        void setCaptureMode(CaptureMethod mode); /* capture thread */
+        void initCaptureConfig(); /* capture thread */
+        void setCaptureFps(int fps); /* capture thread */
 #if WIN32
-		void updateWindowForDirectXDesktopDuplicationMode(); /* capture thread only */
+        void updateWindowForDirectXDesktopDuplicationMode(); /* capture thread only */
 #endif
-		void pushFrameQueue(shared_ptr<Frame> frame); /* atomic */
-		shared_ptr<Frame> removeFrameQueue(); /* atomic */
+        void pushFrameQueue(shared_ptr<Frame> frame); /* atomic */
+        shared_ptr<Frame> removeFrameQueue(); /* atomic */
 
-	protected:
-		virtual void newFrameArrived(shared_ptr<Frame> frame); /* capture thread only */
-		
-	public:
-		GGCapture() = default;
-		float pixelDensity() const;
-		void setPixelDensity(float density);
-		void setStoragePath(path path); /* atomic */
-		void start(string srchterm, CaptureMethod mode, int fps); /* atomic */
-		void stop(); /* atomic */
-		void showFrame(); /* atomic */
-		void saveFrame(); /* atomic */
-		CaptureStatus status() const; /* atomic */
-	};
+    protected:
+        virtual void newFrameArrived(shared_ptr<Frame> frame); /* capture thread only */
+        
+    public:
+        GGCapture() = default;
+        float pixelDensity() const;
+        void setPixelDensity(float density);
+        void setStoragePath(path path); /* atomic */
+        void start(string srchterm, CaptureMethod mode, int fps); /* atomic */
+        void stop(); /* atomic */
+        void showFrame(); /* atomic */
+        void saveFrame(); /* atomic */
+        CaptureStatus status() const; /* atomic */
+    };
 
 }
